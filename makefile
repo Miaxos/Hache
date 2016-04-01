5a757750172451669481a7b6bcca2813028e0663
Anthony Griffon <anthonygriffon@MacBook-Pro-de-Anthony.local>
2016-04-01 05:17:29 +0200

    Maj

--- makefile
+++ makefile
@@ -1,3 +1,13 @@
+NO_COLOR=\x1b[0m
+OK_COLOR=\x1b[32;01m
+ERROR_COLOR=\x1b[31;01m
+WARN_COLOR=\x1b[33;01m
+
+OK_STRING=$(OK_COLOR)[OK]$(NO_COLOR)
+ERROR_STRING=$(ERROR_COLOR)[ERRORS]$(NO_COLOR)
+WARN_STRING=$(WARN_COLOR)[WARNINGS]$(NO_COLOR)
+
+
 all: test myls mydu myls2
 
 myls: myls.o
@@ -8,19 +18,28 @@ mydu: mydu.o
 
 test: main.o getInput.o functions.o socket.o
 	gcc -o ./bin/test ./build/main.o ./build/getInput.o ./build/functions.o
+	@echo "\033[33;32m\t✓ Build: done."
+	@echo "\033[33;00m=== Compilation in debug mode\t\t\tDONkjn"
 
 main.o: ./src/main.c ./inc/getInput.h ./inc/functions.h
 	gcc -Wall -c ./src/main.c -o ./build/main.o
+	@echo "\033[33;32m\t✓ Build: main done."
+	@echo "\033[33;00m"
 
 getInput.o: ./src/getInput.c
 	gcc -Wall -c ./src/getInput.c -o ./build/getInput.o
+	@echo "\033[33;32m\t✓ Build: input done."
+	@echo "\033[33;00m"
 
 functions.o: ./src/functions.c
-	gcc -Wall -c ./src/functions.c -o ./build/functions.o 
+	gcc -Wall -c ./src/functions.c -o ./build/functions.o
+	@echo "\033[33;32m\t✓ Build: function done."
+	@echo "\033[33;00m"
 
 socket.o: ./src/socket.c
 	gcc -Wall -c ./src/socket.c -o ./build/socket.o
-
+	@echo "\033[33;32m\t✓ Build: socket done."
+	@echo "\033[33;00m"
 myls.o: ./commands/myls.c
 	gcc -c ./commands/myls.c -o ./build/myls.o -w
 
--- src/main.c
+++ src/main.c
@@ -4,7 +4,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
-# include <stdlib.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h> // Pour OS X
 
 
 #include "../inc/getInput.h"
@@ -52,6 +55,79 @@ int main() {
 	} while(1);
 
 	return 0;
+}
+
+int creationSocket() {
+	
+	// Fonction de création de socket
+	// Création d'un socket d'écoute qui va ensuite traiter les infos
+	int my_socket, new_socket, clilen;
+	int port;
+	char buffer[256];
+	struct sockaddr_in server, client;
+	int  n;
+	
+	// Creation du socket
+	my_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// En cas d'erreur
+	if (my_socket < 0) {
+		perror("Erreur dans \'ouverture du socket.");
+		exit(1);
+	}
+	
+	// Structure du socket	
+	port = 10002;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(port);
+	
+	// Bind de l'adresse
+	if (bind(my_socket, (struct sockaddr *) &server, sizeof(server)) < 0) {
+		perror("Erreur: Bind failed.");
+		exit(1);
+	}
+		
+	// Mode écoute (second argument: la queue).
+	listen(my_socket,5);
+		
+	//On accepte les connections
+	puts("Waiting for incoming connections...");
+	clilen = sizeof(struct sockaddr_in);
+	while(1)
+	{
+		new_socket = accept(my_socket, (struct sockaddr *)&client, (socklen_t*)&clilen);
+		if (new_socket<0)
+		{
+			perror("Erreur: Accept failed.");
+			exit(1);
+		}
+
+		/* If connection is established then start communicating */
+		bzero(buffer,256);
+		n = read( new_socket,buffer,255 );
+		
+		if (n < 0) {
+			perror("Erreur: lecture depuis le socket");
+			exit(1);
+		}
+		
+		// On récupère le message
+		// On redirige la sortie standard dup2()
+		// On le traite
+		// On balance
+		printf("Message: %s\n",buffer);
+		
+		/* Write a response to the client */
+		n = write(new_socket,"I got your message",18);
+		
+		if (n < 0) {
+			perror("ERROR writing to socket");
+			exit(1);
+		}
+	}   
+	return 0;
+
 }
 
 
